877217fd9dbd004c25298174c182dbd022d594ab
ap8114 <amanpatidar086@gmail.com>
2026-06-15 13:40:15 +0530

    commodity order fix internal server

--- src/controllers/tradeController.js
+++ src/controllers/tradeController.js
@@ -6,6 +6,8 @@ const { invalidateCache } = require('../utils/cacheManager');
 const { getMcxBaseScrip, getLotSize } = require('../utils/symbolHelper');
 const { buildTradeLog } = require('../utils/logFormatter');
 const MarginService = require('../services/MarginService');
+const TradeService = require('../services/TradeService');
+const tradeService = new TradeService();
 
 const syncPaperPosition = async (userId, symbol, connection = db) => {
     try {
